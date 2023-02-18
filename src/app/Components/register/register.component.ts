d1969ce6fcf5f61ac37018b87de2e1d2006cf961
Achintya00 <55224555+Achintya00@users.noreply.github.com>
2023-02-18 20:31:20 +0530

    v4.1 updated with validations

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,15 +1,31 @@
+import { NgForm } from '@angular/forms';
 import { ProductDetailsService } from './../../Services/product-details.service';
 import { user } from './user.model';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, AfterViewInit {
+  @ViewChild('registerForm')
+  registerForm: any;
+  //@ViewChild('inputFeild', { read: ElementRef }) inputFeild: any;
+  ngAfterViewInit(): void {
+    // if (this.inputFeild.nativeElement.value === '') {
+    //   this.sol = false;
+    //   console.log(this.sol);
+    // }
+    console.log(this.registerForm.form.errors?.['maxlength']);
+  }
   hide = true;
-
   ad: user = {
     firstName: '',
     lastName: '',
@@ -18,6 +34,7 @@ export class RegisterComponent {
     username: '',
     password: '',
   };
+  ngOnInit(): void {}
   constructor(private register: ProductDetailsService) {}
   postData() {
     console.log(this.ad);
