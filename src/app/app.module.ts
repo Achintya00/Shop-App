2a61003888882c626ea46066ee03cc6bfc8e0c6d
Achintya00 <55224555+Achintya00@users.noreply.github.com>
2023-02-18 13:10:50 +0530

    checkbox functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
@@ -31,6 +31,7 @@ import { ProductDetailComponent } from './Components/products/product-detail/pro
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
